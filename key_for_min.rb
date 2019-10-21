5e4d9d36019541d7585b3295ee47b97c8be186a2
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-21 03:30:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect |key, value| do 
+  name_hash.collect |key| do 
     if value == [1] 
       puts "#{key}"
     elsif
