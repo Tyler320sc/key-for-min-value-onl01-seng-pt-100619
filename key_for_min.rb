6d31068c1e05a8068966cdda567cc9907f4439f5
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-21 03:46:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_value = 1100
+  other_value = ""
   name_hash.collect |key, value| do 
-    if value == [1] 
-      puts "#{key}"
-    elsif
-      value == []
-      nil 
+    if value < [1] 
+    
     end 
   end 
 end
